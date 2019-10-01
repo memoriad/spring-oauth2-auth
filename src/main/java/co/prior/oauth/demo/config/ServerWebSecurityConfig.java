287da401cbf37c97bd32bce1e566fb68242e81d8
jamikorn jongka <mmemoriad@gmail.com>
2019-10-01 14:57:08 +0700

    add cors filter

--- src/main/java/co/prior/oauth/demo/config/ServerWebSecurityConfig.java
+++ src/main/java/co/prior/oauth/demo/config/ServerWebSecurityConfig.java
@@ -1,11 +1,14 @@
 package co.prior.oauth.demo.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import co.prior.oauth.demo.filter.CorsConfigFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,4 +25,14 @@ public class ServerWebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return super.authenticationManagerBean();
 	}
 
+	@Bean
+	public FilterRegistrationBean<CorsConfigFilter> requestResponseFilter() {
+		FilterRegistrationBean<CorsConfigFilter> registrationBean = new FilterRegistrationBean<>();
+
+		registrationBean.setFilter(new CorsConfigFilter());
+		registrationBean.addUrlPatterns("/*");
+
+		return registrationBean;
+	}
+	
 }
